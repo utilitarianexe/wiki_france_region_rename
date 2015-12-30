5e7f7bf629312ba6c7e199dca1167f3a197f6a75
Peter <utilitarianexe@gmail.com>
2015-12-29 21:57:42 -0800

    now got the list of all these things now to press go?

--- edit_page.py
+++ edit_page.py
@@ -72,13 +72,34 @@ def edit_markup_arrondissement(article_name):
     if error is not None:
         print(error)
         return
-    
+
     print(new_text)
     edit_page(new_text, article_name, 'testing bot')
+
+def list_of_arrondissements_articles():
+    '''
+    need to get the list of all 300+ of these things
+    not to mention all the other ones we will need
+    
+    currently pulls them out of the markup of the list article on wikipedia
+    saved as a local file(we only plan on doing this once
+
+    Tried various other ways of getting a list 
+    but I think this is going to be our best effort go at this
+    '''
+    article_titles = []
+    with open('list_of_arrondissements_table_markup') as table_file:
+        table_string = table_file.read()
+        links = re.findall(r'\[\[Arrondissement.*\]\]', table_string)
+        for link in links:
+            article_title = link[2:link.index('|')]
+            article_titles.append(article_title)
+    return article_titles
 
 
 
 if __name__ == '__main__':
     # woo finally go one to work on the test wiki
-    article_name = 'Arrondissement of Bourg-en-Bresse'
-    edit_markup_arrondissement(article_name)
+    # article_name = 'Arrondissement of Bourg-en-Bresse'
+    # edit_markup_arrondissement(article_name)
+    list_of_arrondissements_articles()
